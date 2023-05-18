985aa83ae263a45405aa08b41001aa4748565f71
Jerlsn01010 <21564136-Jerlsn01010@users.noreply.replit.com>
2023-05-18 20:54:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import json, requests
+
+print("Hello this program will provide you with the    current temp of an area by providing us with    either a zip code or city.")
+
+print("     Give it a go!")
+
+
+
+#city = str(input("Please provide city name, exp. Vermont  "))
+  
+#zipcde = int(input("Please provide zipcode, exp. 12345  "))  
+base_url = "https://api.openweathermap.org/data/2.5/weather"
+
+appid = "10b06e71194c26fdbb9cee8146f97770"
+#appid = "906b6939735602a519447e37a839d229"
+city = "Detroit"
+#cty = (city)  
+#zcd = (zipcde)
+#cty = (city)
+
+
+#url = f'{base_url}?q={city}zz={zcd}&units=imperial&APPID={appid}'
+url = f"{base_url}?q={city}&units=imperial&APPID={appid}"  
+print(url)
+print()
+
+response = requests.get(url)
+unformated_data =response.json()
+
+temp = unformated_data["main"]["temp"]
+print(f"The current temperature is: {temp}")
+
+temp_max = unformated_data['main']['temp_max']
+print(f"The max temperature is: {temp_max}")
+
+
+
+
+
+
+
+  
